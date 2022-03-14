786cc71e19d3d58491ef6648100fcbde0b7582b4
Marzio95 <marziodellarocca14@gmail.com>
2022-03-14 16:47:20 +0100

    aggiungo la funzione che elimina al click

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 const app = new Vue({
     el: '#root',
     data: {
-        arrayToDo:[
-        {
-            nome: 'fare i compiti',
-            done: false,
+        arrayToDo: [
+            {
+                nome: 'fare i compiti',
+                done: false,
+            },
+            {
+                nome: 'fare esercizio js',
+                done: false,
+            },
+            {
+                nome: 'programmare tutto il giorno',
+                done: false,
+            },
+        ]
+
+    },
+    methods: {
+        removeTodo(index) {
+            this.arrayToDo.splice(index, 1)
         },
-        {
-            nome: 'fare esercizio js',
-            done: false,
-        },
-        {
-            nome: 'programmare tutto il giorno',
-            done: false,
-        },
-    ]
 
     }
 })    
