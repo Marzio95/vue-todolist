973733a34fa63bd1942f16fe8a68f9c776708959
Marzio95 <marziodellarocca14@gmail.com>
2022-03-14 16:15:41 +0100

    preparo struttura lavoro

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        arrayToDo:[
+        {
+            nome: 'fare i compiti',
+            done: false,
+        },
+        {
+            nome: 'fare esercizio js',
+            done: false,
+        },
+        {
+            nome: 'programmare tutto il giorno',
+            done: false,
+        },
+    ]
+
+    }
+})    
